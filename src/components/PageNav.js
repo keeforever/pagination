df154cab0a9c5b6e1364f29c8e19e2af9bca2568
keeforever <kaliyamoorthy.2k@gmail.com>
2022-11-07 08:19:17 +0000

    chore: maintenance

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -1,23 +1,38 @@
-import { buttonCounts } from "../utils"
-import useFetch from "../useFetch"
+import { buttonCounts } from "../utils";
+import useFetch from "../useFetch";
 
-const PageNav = ({setPage,nextPage,prevPage,lastIndex}) => {
-  const {apiData} =useFetch();
-  // button array from utils.js
-  const {buttonArr}=buttonCounts(apiData,10)
+const PageNav = ({ setPage, nextPage, prevPage, lastIndex }) => {
+  const { apiData } = useFetch();
+
+  // genarate button array
+  const { buttonArr } = buttonCounts(apiData, 10);
+
   // match page and button
-  const btnMatch=lastIndex/10
+  const btnMatch = lastIndex / 10;
+
   return (
     <div className="page__nav-container">
-      <button className="btn-prev text-btn btn" onClick={()=>prevPage()}>Prev</button>
-      {
-        buttonArr.map((pageNum)=>{
-          return <button className={`${ pageNum===btnMatch ? "btn num-btn page-active":"btn num-btn"}`} key={pageNum} onClick={()=>setPage(pageNum)}>{pageNum}</button>
-        })
-      }
-      <button className="btn-next text-btn btn" onClick={()=>nextPage()}>Next</button>
+      <button className="btn-prev text-btn btn" onClick={() => prevPage()}>
+        Prev
+      </button>
+      {buttonArr.map((pageNum) => {
+        return (
+          <button
+            className={`${
+              pageNum === btnMatch ? "btn num-btn page-active" : "btn num-btn"
+            }`}
+            key={pageNum}
+            onClick={() => setPage(pageNum)}
+          >
+            {pageNum}
+          </button>
+        );
+      })}
+      <button className="btn-next text-btn btn" onClick={() => nextPage()}>
+        Next
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default PageNav
+export default PageNav;
